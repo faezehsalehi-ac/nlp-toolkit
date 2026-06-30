8eb6091e3a7f7acd7930c8f86cfe406e6f421012
faezeh salehi1 <salehi-f1@Cyrus-bank.com>
2026-06-30 13:58:46 +0330

    feat: add remove_punctuation method to TextCleaner

--- nlp_toolkit/cleaner.py
+++ nlp_toolkit/cleaner.py
@@ -3,3 +3,6 @@ import re
 class TextCleaner:
     def remove_html(self, text):
         return re.sub(r'<[^>]+>', '', text)
+    
+    def remove_punctuation(self, text):
+        return re.sub(r'[^\w\s]', '', text)
