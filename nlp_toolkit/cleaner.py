8895e974a14aa8b52a52d01c095a9432a7b6f7c0
faezeh salehi1 <salehi-f1@Cyrus-bank.com>
2026-06-30 14:49:56 +0330

    feat: add clean method to TextCleaner

--- nlp_toolkit/cleaner.py
+++ nlp_toolkit/cleaner.py
@@ -12,3 +12,10 @@ class TextCleaner:
     
     def to_lowecase(self, text):
         return text.lower()
+    
+    def clean(self, text):
+        text = self.remove_html(text)
+        text = self.remove_numbers(text)
+        text = self.remove_punctuation(text)
+        text = self.to_lowecase(text)
+        return text
