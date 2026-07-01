f163a7e6f56b313bc3f0388c0eaa264ffe752120
faezehsalehi-ac <faezehsalehi.ac@gmail.com>
2026-07-01 07:53:21 +0330

    feat: update clean method in textcleaner

--- nlp_toolkit/cleaner.py
+++ nlp_toolkit/cleaner.py
@@ -10,14 +10,18 @@ class TextCleaner:
     def remove_numbers(self, text):
         return re.sub(r'\d+', '', text)
     
-    def to_lowecase(self, text):
+    def to_lowercase(self, text):
         return text.lower()
     
-    def clean(self, text):
+    def clean(self, text, remove_numbers=False):
         text = self.remove_html(text)
-        text = self.remove_numbers(text)
+        
+        if remove_numbers:
+            text = self.remove_numbers(text)
+
+        
         text = self.remove_punctuation(text)
-        text = self.to_lowecase(text)
+        text = self.to_lowercase(text)
         text = self.remove_extra_spaces(text)
         return text
     
